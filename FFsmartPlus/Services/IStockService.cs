d285b56aeaec06d069a9323c0163d4db064c41a5
Nicholas McCaig <nick.mccaig@hotmail.co.uk>
2023-01-18 15:55:22 +0000

    Moved Stock Logic to StockService from controller

--- FFsmartPlus/Services/IStockService.cs
+++ FFsmartPlus/Services/IStockService.cs
@@ -5,5 +5,6 @@ namespace FFsmartPlus.Services;
 public interface IStockService
 {
     public Task<bool> AddStock(long id, NewUnitDto newUnits, string username);
+    public Task<bool> RemoveStock(long id,double Quantity, string UserName);
 
 }
